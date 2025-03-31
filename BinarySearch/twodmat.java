47205fc6eef9d17bc3c489ad64928ceaf4a4a0f0
shivendera <shivendrapartapsingh365@gmail.com>
2025-03-31 18:36:57 +0530

    b

--- BinarySearch/twodmat.java
+++ BinarySearch/twodmat.java
@@ -1,5 +1,24 @@
 package BinarySearch;
 
 public class twodmat {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int n=matrix.length;
+        int m=matrix[0].length;
+        int st=0, end=n*m-1;
+        while(st<=end){
+            int mid=st+(end-st)/2;
+            int midelt=matrix[mid/m][mid%m];
+            if(midelt==target) return true;
+            if(target<midelt){
+                end=mid-1;
+            }
+            else{
+                st=mid+1;
+            }
+        }
+        return false;
+
+        
+    }
     
 }
