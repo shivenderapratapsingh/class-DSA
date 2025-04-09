1587c0844329a26eb15f23623aa2c737997f7e79
shivendera <shivendrapartapsingh365@gmail.com>
2025-04-10 00:27:56 +0530

    a

---
+++ BitManipulation/ConvertToBinary3.java
@@ -0,0 +1,9 @@
+package BitManipulation;
+
+public class ConvertToBinary3 {
+    public static void main(String[] args) {
+        int n=13;
+        String binary=Integer.toBinaryString(n);
+        System.out.println(binary);
+    }
+}
