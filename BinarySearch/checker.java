aedec532ef4f34c90bcecfd7b3c08d4a30547127
shivendera <shivendrapartapsingh365@gmail.com>
2025-04-14 23:32:22 +0530

    a

--- BinarySearch/checker.java
+++ BinarySearch/checker.java
@@ -1,6 +1,8 @@
 package BinarySearch;
 public class checker {
     public static void main(String[] args) {
-        
+        System.out.println();
+        System.out.println("Binary Search Checker");
+        System.out.println("This program checks if the binary search algorithm is implemented correctly.");
     }
 }
