96d196b531d1e8a37eb983233881df53b6215e0e
shivendera <shivendrapartapsingh365@gmail.com>
2025-04-14 23:32:51 +0530

    a

--- BinarySearch/checker.java
+++ BinarySearch/checker.java
@@ -4,5 +4,9 @@ public class checker {
         System.out.println();
         System.out.println("Binary Search Checker");
         System.out.println("This program checks if the binary search algorithm is implemented correctly.");
+        System.out.println("It will generate a random array of integers and search for a target value.");
+        System.out.println("If the target value is found, it will print the index of the target value.");
+        System.out.println("If the target value is not found, it will print -1.");
+        System.out.println("It will also check if the target value is in the array and if the index is correct.");
     }
 }
