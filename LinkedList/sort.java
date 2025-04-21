c26daae998d60b711d371d4f7a7f53f7f3a8da41
shivendera <shivendrapartapsingh365@gmail.com>
2025-04-21 09:49:40 +0530

    a

---
+++ LinkedList/sort.java
@@ -0,0 +1,37 @@
+package LinkedList;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+class Node{
+    int data;
+    Node next;
+    Node(int data){
+        this.data=data;
+        this.next=null;
+    }
+}
+public class sort {
+     public ListNode sortList(ListNode head) {
+         if (head == null) return null;
+
+        List<Integer> values = new ArrayList<>();
+        ListNode curr = head;
+        while (curr != null) {
+            values.add(curr.data);
+            curr = curr.next;
+        }
+
+       
+        Collections.sort(values);
+
+        ListNode dummy = new ListNode(0);
+        ListNode temp = dummy;
+        for (int val : values) {
+            temp.next = new ListNode(val);
+            temp = temp.next;
+        }
+
+        return dummy.next;
+    }
+}
