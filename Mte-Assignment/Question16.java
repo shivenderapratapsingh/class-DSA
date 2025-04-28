ffbe0a7047e14249a66c4dc1db9c94b61139aa43
shivendera <shivendrapartapsingh365@gmail.com>
2025-04-28 23:39:58 +0530

    a

--- Mte-Assignment/Question16.java
+++ Mte-Assignment/Question16.java
@@ -1,3 +1,24 @@
 public class Question16 {
-    
-}
+  
+        public int majorityElement(int[] nums) {
+            int count=0;
+            int element=-1;
+            for(int i=0;i<nums.length;i++){
+                if(count==0){
+                    count=1;
+                    element=nums[i];
+                }
+                else if(element==nums[i]) count ++;
+                else{
+                    count--;
+                }
+            }
+            int ans=0;
+            for(int i:nums){
+                if(i==element) ans++;
+            }
+            if(ans>nums.length/2) return element;
+            return -1;
+        }
+    }
+
