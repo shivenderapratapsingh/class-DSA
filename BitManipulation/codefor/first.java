8470c63d38812a96aa98d8139131cf80cf8dd0c3
shivendera <shivendrapartapsingh365@gmail.com>
2025-05-18 09:16:44 +0530

    a

---
+++ BitManipulation/codefor/first.java
@@ -0,0 +1,11 @@
+package BitManipulation.codefor;
+
+public class first {
+    public static String hi(int [] arr){
+        
+    }
+    public static void main(String[] args) {
+        
+
+    }
+}
