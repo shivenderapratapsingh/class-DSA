f0ef58745c0e0eb8358d01117e2200b10a4db2a9
shivendera <shivendrapartapsingh365@gmail.com>
2025-05-29 10:47:00 +0530

    a

--- Recurrsion/printn.java
+++ Recurrsion/printn.java
@@ -1,13 +1,23 @@
 package Recurrsion;
 
 public class printn {
-   public static  void pr(int n){
-        if(n==0) return;
-        pr(n-1);
-        System.out.print(n+" ");
+     static int sum(int n) {
+        if (n < 10) return n;
+         int ans= (n % 10) + sum(n / 10);
+         return ans;
+    }
+
+    static int repeatedSumOfDigits(int N) {
+         int n = sum(N);
+        if (n<= 9) {
+            return n;
+        }
+        
+        return repeatedSumOfDigits(n);
     }
     public static void main(String[] args) {
-       pr(5);
+        int a=repeatedSumOfDigits(987261);
+        System.out.println(a);
     }
     
 }
