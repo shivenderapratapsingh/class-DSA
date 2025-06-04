577522c9b0644cbb2951bf537e3ce729505998f7
shivendera <shivendrapartapsingh365@gmail.com>
2025-06-04 09:44:35 +0530

    a

---
+++ Recurrsion/permutaion.java
@@ -0,0 +1,18 @@
+package Recurrsion;
+
+public class permutaion {
+    static void permute(String s, String curr) {
+        if (s.length() == 0) {
+            System.out.println(curr);
+            return;
+        }
+        for (int i = 0; i < s.length(); i++) {
+            permute(s.substring(0, i) + s.substring(i + 1), curr + s.charAt(i));
+        }
+    }
+
+    public static void main(String[] args) {
+        String s = "abc";
+        permute(s, "");
+    }
+}
