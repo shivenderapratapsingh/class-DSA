3688ff08943659843e981b572207e4da13d97f3a
shivendera <shivendrapartapsingh365@gmail.com>
2025-06-18 20:53:06 +0530

    a

---
+++ Graph/BFS.java
@@ -0,0 +1,77 @@
+package Graph;
+
+
+import java.util.*;
+
+public class BFS {
+
+   
+    public static ArrayList<ArrayList<Integer>> adj(ArrayList<ArrayList<Integer>> ab, int n) {
+        ArrayList<ArrayList<Integer>> ans = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            ans.add(new ArrayList<>());
+        }
+
+        for (ArrayList<Integer> edge : ab) {
+            int u = edge.get(0);
+            int v = edge.get(1);
+
+          
+            ans.get(u).add(v);
+            ans.get(v).add(u);
+        }
+
+        return ans;
+    }
+
+   
+    public static void bfs(ArrayList<ArrayList<Integer>> adj, int start, int n) {
+        boolean[] visited = new boolean[n];
+        Queue<Integer> queue = new LinkedList<>();
+
+        visited[start] = true;
+        queue.offer(start);
+
+        System.out.print("BFS Traversal starting from node " + start + ": ");
+        while (!queue.isEmpty()) {
+            int node = queue.poll();
+            System.out.print(node + " ");
+
+            for (int neighbor : adj.get(node)) {
+                if (!visited[neighbor]) {
+                    visited[neighbor] = true;
+                    queue.offer(neighbor);
+                }
+            }
+        }
+        System.out.println();
+    }
+
+    // Main method
+    public static void main(String[] args) {
+        ArrayList<ArrayList<Integer>> edges = new ArrayList<>();
+        edges.add(new ArrayList<>(Arrays.asList(0, 1)));
+        edges.add(new ArrayList<>(Arrays.asList(0, 2)));
+        edges.add(new ArrayList<>(Arrays.asList(1, 2)));
+        edges.add(new ArrayList<>(Arrays.asList(1, 3)));
+
+        int n = 4;
+
+        ArrayList<ArrayList<Integer>> adjacencyList = adj(edges, n);
+
+        
+        System.out.println("Adjacency List:");
+        for (int i = 0; i < adjacencyList.size(); i++) {
+            System.out.print(i + ": ");
+            for (int v : adjacencyList.get(i)) {
+                System.out.print(v + " ");
+            }
+            System.out.println();
+        }
+
+       
+        bfs(adjacencyList, 0, n);
+    }
+}
+
--- LinkedList/mergeksorted.java
+++ LinkedList/mergeksorted.java
@@ -1,41 +1,41 @@
-package LinkedList;
+// package LinkedList;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-class ListNode{
-    int data;
-    ListNode next;
-    ListNode(int data){
-        this.data=data;
-        this.next=null;
-    }
-}
+// import java.util.ArrayList;
+// import java.util.Collections;
+// import java.util.List;
+// class ListNode{
+//     int data;
+//     ListNode next;
+//     ListNode(int data){
+//         this.data=data;
+//         this.next=null;
+//     }
+// }
 
-public class mergeksorted {
-      public void traverse(ListNode[] lists,List ans) {
-    for (int i = 0; i < lists.length; i++) {
-        ListNode current = lists[i];
-        while (current != null) {
-            ans.add(current.data);
-            current = current.next;
-        }
-    }
-}
+// public class mergeksorted {
+//       public void traverse(ListNode[] lists,List ans) {
+//     for (int i = 0; i < lists.length; i++) {
+//         ListNode current = lists[i];
+//         while (current != null) {
+//             ans.add(current.data);
+//             current = current.next;
+//         }
+//     }
+// }
 
-    public ListNode mergeKLists(ListNode[] lists) {
-        List<Integer> values = new ArrayList<>();
-        traverse(lists,values);
-        Collections.sort(values);
-        ListNode dummy = new ListNode(0);
-        ListNode temp = dummy;
-        for (int val : values) {
-            temp.next = new ListNode(val);
-            temp = temp.next;
-        }
+//     public ListNode mergeKLists(ListNode[] lists) {
+//         List<Integer> values = new ArrayList<>();
+//         traverse(lists,values);
+//         Collections.sort(values);
+//         ListNode dummy = new ListNode(0);
+//         ListNode temp = dummy;
+//         for (int val : values) {
+//             temp.next = new ListNode(val);
+//             temp = temp.next;
+//         }
 
-        return dummy.next;
+//         return dummy.next;
 
         
-    }
-}
+//     }
+// }
